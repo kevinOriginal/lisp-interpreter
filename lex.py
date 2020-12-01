8b52174ba0bcbacd72b9ac71c6c7176d0751b997
ziho <ziho@kakao.com>
2020-12-01 15:00:15 +0900

    lexer

--- lex.py
+++ lex.py
@@ -1,34 +1,26 @@
-program = "(begin (define 'r 10) (* pi (* r r)));;;; this is comment"
-
+import re
 
 def tokenize(chars: str):
-    comment = {'comment': chars.split(';', 1)[1]}
-    tokens = chars.split(';')[0].replace('(', ' ( ').replace(')', ' ) ').replace("'", " ' ").split()
+    chars = chars.lower() #String 대소문자를 전부 소문자로 처리
+    # comment = {'comment': chars.split(';', 1)[1]} # 주석
+    tokens = chars.split(';')[0].replace('(', ' ( ').replace(')', ' ) ').replace("'", " ' ").split() # 주석 제거
     tokenized = []
     for token in tokens:
-        if token == '(':
-            token = {'Lparen': token}
-        elif token == ')':
-            token = {'Rparen': token}
-        elif token == "'":
-            token = {'quote': token}
-        # elif token in ['begin', 'define', ]:  # 함수명 식별자 값 형식
-            # token = {'Function1': token} #예시
-        # elif token in []:
-            # token = {'Function2': token} #예시
-        elif token == '*':
-            token = {'multiply': token}
-        elif token == '+':
-            token = {'plus': token}
-        elif token == '-':
-            token = {'minus': token}
-        elif token == '/':
-            token = {'divide': token}
+        if token in ['(',')',"'",'*','+','-','/','<','>','<=','>=','$']:
+            token = {token: token}
+        elif token in ['begin', 'define', 'setq', 'car','cdr', 'list', 'nth', 'nil', 'cons','reverse','append','length','member','assoc','remove','subst','atom','null','numberp','zerop','minusp','equal','stringp','if','cond'] :  
+            token = {token.upper() : token.upper()} #함수들을 다 대문자로 토큰화 
+        elif re.compile(r'^ca[d]+r').search(token): #car {cdr}  == ca{d}r , 토큰은 cadr로 
+            token = {'CADR': token.upper()}
         else:
-            token = {'identifier': token}
+            if re.compile(r'^[\d]+.[\d]*$').search(token):
+                token = {'value': token} 
+            else:
+                token = {'id': token}
         tokenized.append(token)
-    tokenized.append(comment)
     return tokenized
 
 
-print(tokenize(program))
+if __name__ == "__main__":
+    test_text = '(cadddr X) ;this is comment'
+    print(tokenize(test_text))

02859e395fcc39f1f89671cc588031be868cbe94
Kevin Jin <tost8295@gmail.com>
2020-12-05 09:08:06 +0900

    Add basic execeptions

---
+++ exceptions.py
@@ -0,0 +1,27 @@
+class BaseException(Exception):
+    def __init__(self, message):
+        self.message = message
+
+    def __str__(self):
+        return "[RuntimeError] : %s" % self.message
+
+
+class ArgumentsError(BaseException):
+    def __str__(self):
+        return "[ArgumentsError] : %s" % self.message
+
+
+class DuplicateError(BaseException):
+    def __str__(self):
+        return "[DuplicateError] : %s" % self.messag
+
+
+class UndefinedError(BaseException):
+    def __str__(self):
+        return "[UndefinedError] : %s" % self.message
+
+class TypeError(BaseException):
+    def __str__(self):
+          return "[TypeError] : %s" % self.message
+
+
